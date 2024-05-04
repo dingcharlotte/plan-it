6c8cde2eaa70e1fc6bdcda70266abaf8e5a40b1b
Charlotte Ding <cd494@cornell.edu>
2024-05-03 21:05:51 -0400

    Modify Time to one-to-many

--- backend/db.py
+++ backend/db.py
@@ -7,30 +7,40 @@ db = SQLAlchemy()
 association_table = db.Table(
     "association",
     db.Column("user_id", db.Integer, db.ForeignKey("users.id")),
-    db.Column("time_id", db.Integer, db.ForeignKey("times.id"))
-)
-
-association_table = db.Table(
-    "association",
-    db.Column("user_id", db.Integer, db.ForeignKey("users.id")),
     db.Column("event_id", db.Integer, db.ForeignKey("events.id"))
 )
+# association_table = db.Table(
+#     "association",
+#     db.Column("user_id", db.Integer, db.ForeignKey("users.id")),
+#     db.Column("time_id", db.Integer, db.ForeignKey("times.id"))
+# )
 
 # model classes
 class User(db.Model):
     """
     User Model. 
+    Has a one-to-many relationship with the Time model. 
+    Has a many-to-many relationship with the Event model. 
     """
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    username = db.Column(db.String(30), nullable=False)
-    available_times = db.relationship('Time', secondary='user_times', back_populates='users')
-    joined_events = db.relationship('Event', secondary='user_events', back_populates='users')
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    available_times = db.relationship("Time", cascade="delete")
+    # available_times = db.relationship('Time', secondary='user_times', back_populates='users')
+    joined_events = db.relationship("Event", secondary=association_table, back_populates='users')
+    # joined_events = db.relationship("Event", secondary='user_events', back_populates='users')
 
+    def __init__(self, **kwargs): 
+        """
+        Initialize User object. 
+        """
+        self.name = kwargs.get("name", "")
+        self.username = kwargs.get("username", "")
+    
     def serialize(self):
         """
-        Serializes a User object. 
+        Serialize User object. 
         """
         return {
             "id": self.id,
@@ -45,19 +55,29 @@ class Time(db.Model):
     Time Model. 
     """
     __tablename__ = 'times'
-    id = db.Column(db.Integer, primary_key=True)
-    weekday = db.Column(db.String)
-    timeslot = db.Column(db.String(10))
-    users = db.relationship('User', secondary='user_times', back_populates='available_times')
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    weekday = db.Column(db.Integer, nullable=False)
+    timeslot = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    # users = db.relationship('User', secondary='user_times', back_populates='available_times')
 
+    def __init__(self, **kwargs):
+        """
+        Initialize Time object. 
+        """
+        self.weekday = kwargs.get("weekday", 0) # default to Sunday
+        self.timeslot = kwargs.get("timeslot", "")
+        self.user_id = kwargs.get("user_id")
+    
     def serialize(self):
         """
-        Serializes a Time object. 
+        Serialize Time object. 
         """
         return {
             "id": self.id,
             "weekday": self.weekday,
-            "timeslot": self.timeslot
+            "timeslot": self.timeslot, 
+            "user_id": self.user_id
         }
     
     # def simple_serialize(self):
@@ -75,13 +95,19 @@ class Event(db.Model):
     Event Model. 
     """
     __tablename__ = 'events'
-    id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(300))
-    users = db.relationship('User', secondary='user_events', back_populates='joined_events')
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    description = db.Column(db.String, nullable=False)
+    users = db.relationship('User', secondary=association_table, back_populates='joined_events')
 
+    def __init__(self, **kwargs):
+        """
+        Initialize Event object. 
+        """
+        self.description = kwargs.get("description", "")
+        
     def serialize(self):
         """
-        Serializes an Event object.
+        Serialize Event object.
         """
         return {
             "id": self.id,
